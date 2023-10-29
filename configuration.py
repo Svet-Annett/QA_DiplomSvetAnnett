9eeed09c6929a3a45130cc3729e34aa3e04a23d9
Svet-Annett <145775033+Svet-Annett@users.noreply.github.com>
2023-10-29 20:44:51 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL = "https://bdb747b1-a2c3-4ff1-82f8-bfb9a0da1e4c.serverhub.praktikum-services.ru/"
+CREATE_NEW_ORDER = "/api/v1/orders"
+ORDER_TRACK_INFO = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Anna",
+    "lastName": "Svetlova",
+    "address": "Moscow",
+    "metroStation": 4,
+    "phone": "+7 911 234 56 78",
+    "rentTime": 2,
+    "deliveryDate": "2023-11-12",
+    "comment": "Spasibo",
+    "color": [
+        "BLACK"
+    ]
+}
